712672e394091aedbb7222a4de35e1751a3c4bfd
destimon <destaom@gmail.com>
2020-07-15 14:20:12 +0300

    Saga initiation

--- store/index.ts
+++ store/index.ts
@@ -1,13 +1,18 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from './reducers/index';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga'
+
+const sagaMiddleware = createSagaMiddleware()
 
 const makeStore = (initialState: object) => {
   return createStore(
       rootReducer, 
       initialState,
-      composeWithDevTools()
+      composeWithDevTools(applyMiddleware(sagaMiddleware))
     );
 };
+
+sagaMiddleware.run();
 
 export default makeStore;
